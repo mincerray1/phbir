5383636f93e2fb3d134b60168958324d9abaae51
janguluan@servio.ph <janguluan@servio.ph>
2021-08-19 01:44:59 +0800

    2550

--- phbir/ph_localization/bir_forms.py
+++ phbir/ph_localization/bir_forms.py
@@ -82,6 +82,72 @@ def bir_2307(company, supplier, purchase_invoice, from_date, to_date, response_t
 
     return_document(html, filename, options, response_type)
 
+def bir_2307(company, year, month, response_type="pdf"):
+    frappe.has_permission('BIR 2550M', throw=True)
+
+    from phbir.ph_localization.report.bir_2307.bir_2307 import get_data as get_data_bir_2307
+    data = get_data_bir_2307(company, supplier, purchase_invoice, from_date, to_date)
+
+    data_ip = []
+    data_mp = []
+
+    ip_month_1_total = 0
+    ip_month_2_total = 0
+    ip_month_3_total = 0
+    ip_grand_total = 0
+    ip_tax_withheld_for_the_quarter_total = 0
+
+    mp_month_1_total = 0
+    mp_month_2_total = 0
+    mp_month_3_total = 0
+    mp_grand_total = 0
+    mp_tax_withheld_for_the_quarter_total = 0
+
+    for entry in data:
+        if entry.payment_type == 'Income Payment':
+            ip_month_1_total = ip_month_1_total + entry.month_1
+            ip_month_2_total = ip_month_2_total + entry.month_2
+            ip_month_3_total = ip_month_3_total + entry.month_3
+            ip_grand_total = ip_grand_total + entry.total
+            ip_tax_withheld_for_the_quarter_total = ip_tax_withheld_for_the_quarter_total + entry.tax_withheld_for_the_quarter
+
+            data_ip.append(entry)
+        elif entry.payment_type == 'Money Payment':
+            mp_month_1_total = mp_month_1_total + entry.month_1
+            mp_month_2_total = mp_month_2_total + entry.month_2
+            mp_month_3_total = mp_month_3_total + entry.month_3
+            mp_grand_total = mp_grand_total + entry.total
+            mp_tax_withheld_for_the_quarter_total = mp_tax_withheld_for_the_quarter_total + entry.tax_withheld_for_the_quarter
+
+            data_mp.append(entry)
+
+    context = {
+        'from_date': getdate(from_date),
+        'to_date': getdate(to_date),
+        'payor': get_company_information(company),
+        'payee': get_supplier_information(supplier),
+        'data_ip': data_ip,
+        'ip_month_1_total': ip_month_1_total,
+        'ip_month_2_total': ip_month_2_total,
+        'ip_month_3_total': ip_month_3_total,
+        'ip_grand_total': ip_grand_total,
+        'ip_tax_withheld_for_the_quarter_total': ip_tax_withheld_for_the_quarter_total,
+        'data_mp': data_mp,
+        'mp_month_1_total': mp_month_1_total,
+        'mp_month_2_total': mp_month_2_total,
+        'mp_month_3_total': mp_month_3_total,
+        'mp_grand_total': mp_grand_total,
+        'mp_tax_withheld_for_the_quarter_total': mp_tax_withheld_for_the_quarter_total
+    }
+
+    filename = "BIR 2307"
+    
+    context["build_version"] = frappe.utils.get_build_version()
+    html = frappe.render_template("templates/bir_forms/bir_2307_template.html", context)
+    options["page-size"] = "Legal"
+
+    return_document(html, filename, options, response_type)
+
 @frappe.whitelist()
 def return_document(html, filename="document", options=options, response_type="download"):
     frappe.local.response.filename = "{filename}.pdf".format(filename=filename)
