b22f1e856656cd48ea6b70d38116da5bb94b9ce7
janguluan@servio.ph <janguluan@servio.ph>
2021-09-03 16:23:47 +0800

    cdj, crj init

---
+++ phbir/ph_localization/report/boa_cash_receipts_journal/boa_cash_receipts_journal.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, SERVIO Technologies and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["BOA Cash Receipts Journal"] = {
+	"filters": [
+
+	]
+};
