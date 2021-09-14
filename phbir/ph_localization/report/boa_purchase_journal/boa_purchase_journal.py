663a6028c0079d8181945c820dd2f4faa24bd570
janguluan@servio.ph <janguluan@servio.ph>
2021-09-14 18:37:13 +0800

    pj with return

--- phbir/ph_localization/report/boa_purchase_journal/boa_purchase_journal.py
+++ phbir/ph_localization/report/boa_purchase_journal/boa_purchase_journal.py
@@ -12,7 +12,9 @@ def execute(filters=None):
     return columns, data
 
 def get_data(filters):
-    result = frappe.db.sql("""
+    result = []
+
+    data_pi = frappe.db.sql("""
     SELECT 
         pi.posting_date, 
         (CASE WHEN IFNULL(pi.tax_id, '') = '' THEN s.tax_id ELSE pi.tax_id END) AS tax_id,
@@ -71,9 +73,74 @@ def get_data(filters):
         and pi.posting_date >= %s
         and pi.posting_date <= %s
         and pi.company = %s
-    ORDER BY
-        pi.posting_date ASC
     """, (getdate(filters.from_date), getdate(filters.to_date), filters.company), as_dict=1)
+
+    result.extend(data_pi)
+
+    # separate return query for simplicity
+    data_return = frappe.db.sql("""
+    SELECT 
+        pi.posting_date, 
+        (CASE WHEN IFNULL(pi.tax_id, '') = '' THEN s.tax_id ELSE pi.tax_id END) AS tax_id,
+        pi.supplier,
+        s.supplier_name,
+        /* pi.supplier_address, */
+        pi.name,
+        pi.currency,
+        pi.is_return,
+        pi.bill_no,
+        (pi.base_net_total + pi.base_discount_amount) as total,
+        (pi.base_discount_amount) as net_discount,
+        IFNULL(-ptac_totals.base_taxes_and_charges_added, 0) as tax_amount,
+        IFNULL(-ptac_totals.base_taxes_and_charges_deducted, 0) as withholding_tax_amount,
+        pi.base_grand_total as grand_total
+    FROM
+        `tabPurchase Invoice` pi
+    LEFT JOIN
+        `tabSupplier` s
+    ON 
+        pi.supplier = s.name
+    LEFT JOIN
+        (
+            SELECT 
+                ptac.parent,
+                SUM(ABS(
+                    CASE 
+                    WHEN (ptac.base_tax_amount < 0 and ptac.add_deduct_tax = 'Add') or (ptac.base_tax_amount > 0 and ptac.add_deduct_tax = 'Deduct') 
+                        THEN ptac.base_tax_amount 
+                    ELSE 
+                        0 
+                    END)) AS base_taxes_and_charges_added,
+                SUM(ABS(
+                    CASE 
+                    WHEN (ptac.base_tax_amount > 0 and ptac.add_deduct_tax = 'Add') or (ptac.base_tax_amount < 0 and ptac.add_deduct_tax = 'Deduct') 
+                        THEN ptac.base_tax_amount 
+                    ELSE 
+                        0 
+                    END)) AS base_taxes_and_charges_deducted
+            FROM
+                `tabPurchase Taxes and Charges` ptac
+            /*LEFT JOIN
+                `tabPurchase Invoice` parent
+            ON
+                ptac.parent = parent.name
+            WHERE
+                parent.docstatus = 1
+                and parent.is_return = 0*/
+            GROUP BY ptac.parent
+        ) AS ptac_totals
+    ON 
+        pi.name = ptac_totals.parent
+    WHERE
+        pi.docstatus = 1
+        and pi.is_return = 1
+        and pi.posting_date >= %s
+        and pi.posting_date <= %s
+        and pi.company = %s
+    """, (getdate(filters.from_date), getdate(filters.to_date), filters.company), as_dict=1)
+
+    result.extend(data_return)
+    result = sorted(result, key=lambda row: row.posting_date)
 
     return result
 
