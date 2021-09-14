3da7d6cc5119bbc0c4f7f21a711d1f7fd0a044b1
janguluan@servio.ph <janguluan@servio.ph>
2021-09-14 19:14:10 +0800

    gj adjustments

--- phbir/ph_localization/report/boa_general_journal/boa_general_journal.py
+++ phbir/ph_localization/report/boa_general_journal/boa_general_journal.py
@@ -12,7 +12,8 @@ def execute(filters=None):
     return columns, data
 
 def get_data(filters):
-    result = frappe.db.sql("""
+    result = []
+    data_all = frappe.db.sql("""
     SELECT 
         ge.posting_date,
         ge.voucher_type,
@@ -33,9 +34,48 @@ def get_data(filters):
         and ge.posting_date >= %s
         and ge.posting_date <= %s
         and ge.company = %s
+        and ge.voucher_type not in ('Sales Invoice', 'Delivery Note', 'Purchase Invoice', 'Purchase Receipt', 'Stock Entry', 'Payment Entry', 'Loan Disbursement', 'Loan Repayment')
     ORDER BY
         ge.posting_date ASC, ge.voucher_no, (CASE WHEN ge.debit > 0 THEN 0 ELSE 1 END) ASC, ge.creation ASC
     """, (getdate(filters.from_date), getdate(filters.to_date), filters.company), as_dict=1)
+
+    result.extend(data_all)
+
+    data_internal_payment_entry = frappe.db.sql("""
+    SELECT 
+        ge.posting_date,
+        ge.voucher_type,
+        ge.voucher_no,
+        (CASE WHEN UPPER(ge.remarks) = 'NO REMARKS' THEN '' ELSE ge.remarks END) AS remarks,
+        a.account_number,
+        a.account_name,
+        ge.debit,
+        ge.credit
+    FROM 
+        `tabGL Entry` ge
+    LEFT JOIN
+        `tabAccount` a
+    ON
+        ge.account = a.name
+    INNER JOIN
+        `tabPayment Entry` pe
+    ON
+        ge.voucher_no = pe.name
+    WHERE
+        ge.docstatus = 1
+        and ge.voucher_type in ('Payment Entry')
+        and pe.payment_type = 'Internal Transfer'
+        and ge.posting_date >= %s
+        and ge.posting_date <= %s
+        and ge.company = %s
+    ORDER BY
+        ge.posting_date ASC, ge.voucher_no, (CASE WHEN ge.debit > 0 THEN 0 ELSE 1 END) ASC, ge.creation ASC
+    """, (getdate(filters.from_date), getdate(filters.to_date), filters.company), as_dict=1)
+
+    result.extend(data_internal_payment_entry)
+    result = sorted(result, key=lambda row: row.posting_date)
+
+    # dunning ? invoice discounting ?
 
     return result
 
