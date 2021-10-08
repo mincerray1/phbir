7c932d40d250f471c157f8f8aa277478ba591342
janguluan@servio.ph <janguluan@servio.ph>
2021-10-08 18:09:27 +0800

    relief prelims

--- phbir/ph_localization/report/relief_summary_list_of_sales/relief_summary_list_of_sales.js
+++ phbir/ph_localization/report/relief_summary_list_of_sales/relief_summary_list_of_sales.js
@@ -2,8 +2,33 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["RELIEF Summary List of Sales"] = {
-	"filters": [
+{% include 'phbir/public/js/utils.js' %}
 
-	]
+frappe.query_reports["RELIEF Summary List of Sales"] = {
+    "filters": [
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+            "reqd": 1
+		},
+		{
+			"fieldname":"year",
+			"label": __("Year"),
+            "fieldtype": "Select",
+            "options": get_years(),
+            "default": moment(frappe.datetime.get_today()).year(),
+            "reqd": 1
+		},
+		{
+			"fieldname":"month",
+			"label": __("Month"),
+			"fieldtype": "Select",
+			"options": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+            "default": moment(frappe.datetime.get_today()).month(),
+            "reqd": 1
+		},
+	],
 };
--- phbir/ph_localization/report/relief_summary_list_of_sales/relief_summary_list_of_sales.py
+++ phbir/ph_localization/report/relief_summary_list_of_sales/relief_summary_list_of_sales.py
@@ -1,8 +1,71 @@
 # Copyright (c) 2013, SERVIO Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
+from frappe.utils import flt, getdate
+from phbir.ph_localization.utils import get_company_information, get_customer_information
+from phbir.ph_localization.bir_forms import return_document
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    columns, data = [], []
+
+    data = get_data(filters.company, filters.year, filters.month)
+    columns = get_columns()
+
+    return columns, data
+
+def get_data(company, year, month):
+    result = []
+
+    return result
+
+def get_columns():
+    columns = [
+        {
+            "fieldname": "tin_with_dash",
+            "label": _("TIN"),
+            "fieldtype": "Data",
+            "width": 180
+        },
+        {
+            "fieldname": "registered_name",
+            "label": _("Registered Name"),
+            "fieldtype": "Link",
+            "options": "Customer",
+            "width": 200
+        },
+        {
+            "fieldname": "individual",
+            "label": _("Individual"),
+            "fieldtype": "Data",
+            "width": 220
+        },
+        {
+            "fieldname": "atc",
+            "label": _("ATC"),
+            "fieldtype": "Link",
+            "options": "ATC",
+            "width": 80
+        },
+        {
+            "fieldname": "income_payment",
+            "label": _("Income Payment"),
+            "fieldtype": "Currency",
+            "width": 200
+        },
+        {
+            "fieldname": "formatted_atc_rate",
+            "label": _("Tax Rate"),
+            "fieldtype": "Data",
+            "width": 80
+        },
+        {
+            "fieldname": "tax_withheld",
+            "label": _("Tax Withheld"),
+            "fieldtype": "Currency",
+            "width": 200
+        },
+    ]
+
+    return columns
