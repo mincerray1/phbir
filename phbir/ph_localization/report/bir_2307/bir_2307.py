cab6b9fe0f0eae94d83a6fcb4336a86085ebf6d4
janguluan@servio.ph <janguluan@servio.ph>
2021-10-18 18:38:39 +0800

    2307 grouped

--- phbir/ph_localization/report/bir_2307/bir_2307.py
+++ phbir/ph_localization/report/bir_2307/bir_2307.py
@@ -29,89 +29,131 @@ def get_data(company, supplier, doctype, purchase_invoice, payment_entry, from_d
 
     if not payment_entry:
         data_pi = frappe.db.sql("""
-            SELECT 
-                (CASE 
-                    WHEN ptac.atc IN (SELECT atc FROM `tabATC` WHERE LEFT(atc, 2) IN ('WI', 'WC')) 
-                        THEN 'Income Payment' 
-                    WHEN ptac.atc IN (SELECT atc FROM `tabATC` WHERE LEFT(atc, 2) IN ('WB', 'WV'))  
-                        THEN 'Money Payment' 
-                END) AS payment_type,
-                ptac.atc AS atc,
-                pi.name AS document_no,
-                (CASE WHEN MONTH(pi.posting_date) IN (1, 4, 7, 10) THEN pi.base_total ELSE 0 END) AS month_1,
-                (CASE WHEN MONTH(pi.posting_date) IN (2, 5, 8, 11) THEN pi.base_total ELSE 0 END) AS month_2,
-                (CASE WHEN MONTH(pi.posting_date) IN (3, 6, 9, 12) THEN pi.base_total ELSE 0 END) AS month_3,
-                pi.base_total AS total,
-                ABS(ptac.base_tax_amount) AS tax_withheld_for_the_quarter
-            FROM 
-                `tabPurchase Invoice` pi
-            LEFT JOIN
-                `tabPurchase Taxes and Charges` ptac
-            ON
-                pi.name = ptac.parent
-            LEFT JOIN 
-                `tabSupplier` as s
-            ON
-                pi.supplier = s.name
-            INNER JOIN
-                `tabAccount` a
-            ON
-                ptac.account_head = a.name
-            WHERE
-                pi.docstatus = 1
-                and pi.is_return = 0
-                and ((ptac.base_tax_amount < 0 and ptac.add_deduct_tax != 'Deduct') or (ptac.base_tax_amount >= 0 and ptac.add_deduct_tax = 'Deduct'))
-                and ptac.atc IN (SELECT atc FROM `tabATC` WHERE tax_type_code IN ('WE', 'WB', 'WV'))
-                and a.account_type = 'Tax'
-                and pi.supplier = %s
-                and pi.posting_date >= %s
-                and pi.posting_date <= %s
-                and pi.company = %s {0}
+            SELECT
+                temp.payment_type,
+                temp.atc,
+                temp.description,
+                SUM(temp.month_1) AS month_1,
+                SUM(temp.month_2) AS month_2,
+                SUM(temp.month_3) AS month_3,
+                SUM(temp.total) AS total,
+                SUM(temp.tax_withheld_for_the_quarter) AS tax_withheld_for_the_quarter
+            FROM
+            (
+                SELECT 
+                    (CASE 
+                        WHEN ptac.atc IN (SELECT atc FROM `tabATC` WHERE LEFT(atc, 2) IN ('WI', 'WC')) 
+                            THEN 'Income Payment' 
+                        WHEN ptac.atc IN (SELECT atc FROM `tabATC` WHERE LEFT(atc, 2) IN ('WB', 'WV'))  
+                            THEN 'Money Payment' 
+                    END) AS payment_type,
+                    ptac.atc AS atc,
+                    atc.description AS description,
+                    pi.name AS document_no,
+                    (CASE WHEN MONTH(pi.posting_date) IN (1, 4, 7, 10) THEN pi.base_total ELSE 0 END) AS month_1,
+                    (CASE WHEN MONTH(pi.posting_date) IN (2, 5, 8, 11) THEN pi.base_total ELSE 0 END) AS month_2,
+                    (CASE WHEN MONTH(pi.posting_date) IN (3, 6, 9, 12) THEN pi.base_total ELSE 0 END) AS month_3,
+                    pi.base_total AS total,
+                    ABS(ptac.base_tax_amount) AS tax_withheld_for_the_quarter
+                FROM 
+                    `tabPurchase Invoice` pi
+                LEFT JOIN
+                    `tabPurchase Taxes and Charges` ptac
+                ON
+                    pi.name = ptac.parent
+                LEFT JOIN 
+                    `tabSupplier` as s
+                ON
+                    pi.supplier = s.name
+                INNER JOIN
+                    `tabAccount` a
+                ON
+                    ptac.account_head = a.name
+                INNER JOIN
+                    `tabATC` atc
+                ON
+                    ptac.atc = atc.name
+                WHERE
+                    pi.docstatus = 1
+                    and pi.is_return = 0
+                    and ((ptac.base_tax_amount < 0 and ptac.add_deduct_tax != 'Deduct') or (ptac.base_tax_amount >= 0 and ptac.add_deduct_tax = 'Deduct'))
+                    and ptac.atc IN (SELECT atc FROM `tabATC` WHERE tax_type_code IN ('WE', 'WB', 'WV') and form_type = '2307')
+                    and a.account_type = 'Tax'
+                    and pi.supplier = %s
+                    and pi.posting_date >= %s
+                    and pi.posting_date <= %s
+                    and pi.company = %s {0}
+            ) temp
+            GROUP BY
+                temp.payment_type,
+                temp.atc,
+                temp.description
             """.format(pi_condition), (supplier, getdate(from_date), getdate(to_date), company), as_dict=1)
     
         result.extend(data_pi)
     
     if not purchase_invoice:
         data_pe = frappe.db.sql("""
-            SELECT 
-                (CASE 
-                    WHEN atac.atc IN (SELECT atc FROM `tabATC` WHERE LEFT(atc, 2) IN ('WI', 'WC')) 
-                        THEN 'Income Payment' 
-                    WHEN atac.atc IN (SELECT atc FROM `tabATC` WHERE LEFT(atc, 2) IN ('WB', 'WV'))  
-                        THEN 'Money Payment' 
-                END) AS payment_type,
-                atac.atc AS atc,
-                pe.name AS document_no,
-                (CASE WHEN MONTH(pe.posting_date) IN (1, 4, 7, 10) THEN pe.base_paid_amount ELSE 0 END) AS month_1,
-                (CASE WHEN MONTH(pe.posting_date) IN (2, 5, 8, 11) THEN pe.base_paid_amount ELSE 0 END) AS month_2,
-                (CASE WHEN MONTH(pe.posting_date) IN (3, 6, 9, 12) THEN pe.base_paid_amount ELSE 0 END) AS month_3,
-                pe.base_paid_amount AS total,
-                ABS(atac.base_tax_amount) AS tax_withheld_for_the_quarter
-            FROM 
-                `tabPayment Entry` pe
-            LEFT JOIN
-                `tabAdvance Taxes and Charges` atac
-            ON
-                pe.name = atac.parent
-            LEFT JOIN 
-                `tabSupplier` as s
-            ON
-                pe.party = s.name
-            INNER JOIN
-                `tabAccount` a
-            ON
-                atac.account_head = a.name
-            WHERE
-                pe.docstatus = 1
-                and pe.payment_type = 'Pay'
-                and pe.party_type = 'Supplier'
-                and ((atac.base_tax_amount < 0 and atac.add_deduct_tax != 'Deduct') or (atac.base_tax_amount >= 0 and atac.add_deduct_tax = 'Deduct'))
-                and atac.atc IN (SELECT atc FROM `tabATC` WHERE tax_type_code IN ('WE', 'WB', 'WV'))
-                and a.account_type = 'Tax'
-                and pe.party = %s
-                and pe.posting_date >= %s
-                and pe.posting_date <= %s
-                and pe.company = %s {0}
+            SELECT
+                temp.payment_type,
+                temp.atc,
+                temp.description,
+                SUM(temp.month_1) AS month_1,
+                SUM(temp.month_2) AS month_2,
+                SUM(temp.month_3) AS month_3,
+                SUM(temp.total) AS total,
+                SUM(temp.tax_withheld_for_the_quarter) AS tax_withheld_for_the_quarter
+            FROM
+            (
+                SELECT 
+                    (CASE 
+                        WHEN atac.atc IN (SELECT atc FROM `tabATC` WHERE LEFT(atc, 2) IN ('WI', 'WC')) 
+                            THEN 'Income Payment' 
+                        WHEN atac.atc IN (SELECT atc FROM `tabATC` WHERE LEFT(atc, 2) IN ('WB', 'WV'))  
+                            THEN 'Money Payment' 
+                    END) AS payment_type,
+                    atac.atc AS atc,
+                    atc.description AS description,
+                    pe.name AS document_no,
+                    (CASE WHEN MONTH(pe.posting_date) IN (1, 4, 7, 10) THEN pe.base_paid_amount ELSE 0 END) AS month_1,
+                    (CASE WHEN MONTH(pe.posting_date) IN (2, 5, 8, 11) THEN pe.base_paid_amount ELSE 0 END) AS month_2,
+                    (CASE WHEN MONTH(pe.posting_date) IN (3, 6, 9, 12) THEN pe.base_paid_amount ELSE 0 END) AS month_3,
+                    pe.base_paid_amount AS total,
+                    ABS(atac.base_tax_amount) AS tax_withheld_for_the_quarter
+                FROM 
+                    `tabPayment Entry` pe
+                LEFT JOIN
+                    `tabAdvance Taxes and Charges` atac
+                ON
+                    pe.name = atac.parent
+                LEFT JOIN 
+                    `tabSupplier` as s
+                ON
+                    pe.party = s.name
+                INNER JOIN
+                    `tabAccount` a
+                ON
+                    atac.account_head = a.name
+                INNER JOIN
+                    `tabATC` atc
+                ON
+                    atac.atc = atc.name
+                WHERE
+                    pe.docstatus = 1
+                    and pe.payment_type = 'Pay'
+                    and pe.party_type = 'Supplier'
+                    and ((atac.base_tax_amount < 0 and atac.add_deduct_tax != 'Deduct') or (atac.base_tax_amount >= 0 and atac.add_deduct_tax = 'Deduct'))
+                    and atac.atc IN (SELECT atc FROM `tabATC` WHERE tax_type_code IN ('WE', 'WB', 'WV') and form_type = '2307')
+                    and a.account_type = 'Tax'
+                    and pe.party = %s
+                    and pe.posting_date >= %s
+                    and pe.posting_date <= %s
+                    and pe.company = %s {0}
+            ) temp
+            GROUP BY
+                temp.payment_type,
+                temp.atc,
+                temp.description
             """.format(pe_condition), (supplier, getdate(from_date), getdate(to_date), company), as_dict=1)
     
         result.extend(data_pe)
@@ -134,8 +176,8 @@ def get_columns():
             "width": 60
         },
         {
-            "fieldname": "document_no",
-            "label": _("Document No"),
+            "fieldname": "description",
+            "label": _("Description"),
             "fieldtype": "Data",
             "width": 150
         },
